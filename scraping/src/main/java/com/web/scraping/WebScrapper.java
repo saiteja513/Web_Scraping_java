02aeb361e6aa3f97109c2e47fedd600b8391d0b8
teja <steja3351@gmail.com>
2023-09-06 00:54:08 +0530

    getting all the titles

--- scraping/src/main/java/com/web/scraping/WebScrapper.java
+++ scraping/src/main/java/com/web/scraping/WebScrapper.java
@@ -2,6 +2,8 @@ package com.web.scraping;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 
 public class WebScrapper {
@@ -9,11 +11,21 @@ public class WebScrapper {
 	public static void main(String[] args) {
 		try {
 			String url = "https://news.ycombinator.com/";
+			
 			Document doc = Jsoup.connect(url).get();
 			// This line will be printing the whole page html for us
 			System.out.println(doc.toString());
 			
-            
+			Elements titleLines = doc.select("span.titleline");
+			//This will be getting all the titles present in ycombinator page
+			System.out.println("This is printing all the titles in ycombinator page. :) ");
+			
+			int i=1;
+            for (Element titleLine : titleLines) {
+                System.out.println("Title " +i+" : "+ titleLine.text());
+                i++;
+            }
+			System.out.println("Done getting all the titles");
 		}
 		catch(Exception e) {
 			e.printStackTrace();
