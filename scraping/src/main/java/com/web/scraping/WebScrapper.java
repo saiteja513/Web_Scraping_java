c7c162a226924ca9759cf133aff74f41310b9031
teja <steja3351@gmail.com>
2023-09-06 00:51:26 +0530

    included jsoup in the clss

--- scraping/src/main/java/com/web/scraping/WebScrapper.java
+++ scraping/src/main/java/com/web/scraping/WebScrapper.java
@@ -1,10 +1,23 @@
 package com.web.scraping;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+
 public class WebScrapper {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		try {
+			String url = "https://news.ycombinator.com/";
+			Document doc = Jsoup.connect(url).get();
+			// This line will be printing the whole page html for us
+			System.out.println(doc.toString());
+			
+            
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
